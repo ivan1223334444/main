7a3b0d2e18b8a6b0d9195b5d5056d5da3ee9bccc
ivan1223334444 <sejkoivan78@gmail.com>
2021-04-11 11:49:25 +0300

    Добавьте файлы проекта.

---
+++ main/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 100;
+	for (int i = 0; i < a; i++)
+	{
+		cout << "hello team! \n";
+		a++;
+	}
+	system("pause");
+	return 0;
+
+}
